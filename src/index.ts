c4a754cee3c7fc1b9df78bac8af33def7c3fcf0e
Yusuke Kamiyamane <y@y14e.com>
2026-05-27 12:56:29 +0900

    v1.2.11

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * Accordion
  * WAI-ARIA compliant accordion pattern implementation in TypeScript.
  *
- * @version 1.2.10
+ * @version 1.2.11
  * @author Yusuke Kamiyamane
  * @license MIT
  * @copyright Copyright (c) Yusuke Kamiyamane
@@ -173,6 +173,7 @@ export default class Accordion {
     saveAttributes(this.#triggerElements, [
       'aria-controls',
       'aria-disabled',
+      'aria-expanded',
       'aria-label',
       'id',
       'style',
