8147889a003ebda9ea120f2afdc6d1d3d3a0f52f
Yusuke Kamiyamane <y@y14e.com>
2026-09-16 18:57:04 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * Accordion
  * WAI-ARIA compliant accordion pattern implementation in TypeScript.
  *
- * @version 2.1.0
+ * @version 2.1.1
  * @author Yusuke Kamiyamane
  * @license MIT
  * @copyright Copyright (c) Yusuke Kamiyamane
